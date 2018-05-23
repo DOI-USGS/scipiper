474514d43bf71126a7db99b4d22646300e27d6ff
Alison Appling <aappling@usgs.gov>
2018-05-23 15:04:35 -0700

    add list_all_targets()

--- R/scmake.R
+++ R/scmake.R
@@ -272,6 +272,54 @@ as_data_file <- function(ind_file, ind_ext=getOption("scipiper.ind_ext")) {
   tools::file_path_sans_ext(ind_file)
 }
 
+#' Get a list of all targets in a remake file
+#'
+#' @param remake_file filename of the remake YAML file from which targets should
+#'   be collected
+#' @param recursive logical. if TRUE, result will include all targets from any
+#'   YAMLs listed in the include: section of the given remake_file, or any YAMLs
+#'   listed in the include: sections of those included YAMLs, etc.
+#' @return vector of all target names explicitly declared in this remake_file
+#'   (and if recursive=TRUE, also the names of targets declared in remake files
+#'   included by this remake_file)
+#' @export
+#' @examples
+#' \dontrun{
+#' # assuming you have a file named remake.yml:
+#' list_all_targets() # get status for all explicitly named targets in remake.yml
+#' 
+#' # status for all targets in a different remake YAML:
+#' list_all_targets('other_remake.yml')
+#' 
+#' # status for all targets in remake.yml and any remake YAMLs included by remake.yml
+#' list_all_targets(recursive=TRUE)
+#' }
+list_all_targets <- function(remake_file=getOption('scipiper.remake_file'), recursive=FALSE) {
+  # load the remake file as a yaml and as remake loads it
+  remake_list <- yaml::yaml.load_file(remake_file)
+  
+  # get all explicitly defined targets
+  targets <- names(remake_list$targets)
+
+  # exclude remake keyword targets, which can be explicit even though they're
+  # usually not
+  targets <- setdiff(targets, c('tidy','clean','purge'))
+  
+  # if requested, include targets of the included ymls
+  if(isTRUE(recursive)) {
+    includes <- remake_list$include
+    nested_targets <- unlist(lapply(includes, list_all_targets))
+    targets <- c(targets, nested_targets)
+  }
+
+  # if we wanted to add more info about these targets, we could return the following instead:
+  # remake_object <- remake:::remake(remake_file=remake_file, verbose=FALSE, load_sources=FALSE)
+  # remake_object$targets[targets]
+  
+  # return a simple vector of target names
+  targets
+}
+
 #' Produce a table describing the remake build status relative to 1+ targets
 #'
 #' @param target_names character vector of targets for which to determine build
