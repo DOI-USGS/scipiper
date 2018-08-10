f86b97adb4a6286322ab8303bcd296375dcfeb8e
Alison Appling <aappling@usgs.gov>
2018-08-10 08:55:25 -0400

    augment & rebuild vignettes

---
+++ inst/doc/shared_cache.R
@@ -0,0 +1,8 @@
+## ----setup, include=FALSE------------------------------------------------
+knitr::opts_chunk$set(echo = TRUE, cache=FALSE, collapse=TRUE)
+knitr::opts_knit$set(root.dir = tempdir())
+
+## ----libs, message=FALSE-------------------------------------------------
+library(scipiper)
+library(dplyr)
+
