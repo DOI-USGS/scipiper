4352ce8b9be4714cc8c5dbec07e96c8d5e0347b4
Platt <lplatt@usgs.gov>
2020-11-20 10:39:52 -0600

    update readr::write_lines fxn according pkg update in v1.4.0

--- tests/testthat/test-combiners.R
+++ tests/testthat/test-combiners.R
@@ -2,7 +2,7 @@ context("Combine task targets into summary files or objects")
 
 test_that("combine_to_tibble works on all reasonable numbers of files", {
   tfiles <- tempfile(pattern=as.character(1:3))
-  for(i in 1:3) readr::write_lines(i, path=tfiles[i])
+  for(i in 1:3) readr::write_lines(i, file=tfiles[i])
   expect_s3_class(combine_to_tibble(), 'tbl_df')
   expect_equal(nrow(combine_to_tibble()), 0)
   expect_equal(combine_to_tibble(tfiles[1])$name, tfiles[1])
